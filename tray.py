01fd982fbeb19cdd2a5f762683305349f8250951
A24 <2823659087@qq.com>
2023-09-02 19:56:51 +0800

    多线程实现托盘图标与20分钟自动更新

--- tray.py
+++ tray.py
@@ -4,12 +4,26 @@ from pystray import MenuItem
 import main
 import win32gui,win32con,win32api
 import time
-
+from threading import Thread
 def init():
     win32api.MessageBox(0,"正在连接卫星...距初始化完成还有30秒","风暴眼",win32con.MB_ICONWARNING)
-
+    threads=[]
+    t1=Thread(target=timer,name="sat",daemon=True)
+    threads.append(t1)
+    t2=Thread(target=icon.run,name="tray")
+    threads.append(t2)
+    print("--------启动-------")
+    for t in threads:
+        t.start()
+    
 status = "正在连接卫星.."
 sat_status="fghj"
+
+def timer():
+    t=time.localtime()
+    while True:
+        auto_set()
+        time.sleep(1200)
 
 def auto_set():
     main.action(1)
@@ -50,6 +64,6 @@ menu = (MenuItem(text='卫星数据更新', action=auto_set),
 image = Image.open("icon.png")
 icon = pystray.Icon("name", image,"风暴眼", menu)
 init()
-auto_set()
-icon.run()
+
+# icon.run()
 
